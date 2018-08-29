3f88711bea1ce1be59d1d169f9c9b8079eca60fd
lprince <lprincewhn@gmail.com>
2018-08-29 10:48:13 +0000

    Add get_flannel_image.sh

--- get_kubeadm_images.sh
+++ get_kubeadm_images.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/bash
 
-OLD_IFS="$IFS"
-for image in `kubeadm config images list --kubernetes-version=v1.11.2`
-do
+function download() {
+    image=$1
+    OLD_IFS="$IFS"
     IFS="/"
     arr=(${image})
+    if [ ${#arr[@]} -eq 3 ];
+    then
+        registry=${arr[0]}/${arr[1]}
+        name_version=${arr[2]}
+    else
+        registry=${arr[0]}
+        name_version=${arr[1]} 
+    fi
     IFS=":"
-    registry=${arr[0]}
-    arr1=(${arr[1]})
-    name=${arr1[0]}
-    tag=${arr1[1]}
+    arr=(${name_version})
+    if [ ${#arr[@]} -eq 2 ];
+    then
+        name=${arr[0]}
+        tag=${arr[1]}
+    else
+        name=${name_version}
+        tag="latest"
+    fi
     echo "docker pull lprince/googlecontainers:${name}-${tag}"
     docker pull lprince/googlecontainers:${name}-${tag}
     echo "docker tag lprince/googlecontainers:${name}-${tag} ${registry}/${name}:${tag}"
     docker tag lprince/googlecontainers:${name}-${tag} ${registry}/${name}:${tag}
+    IFS="$OLD_IFS"
+}
+
+for image in `kubeadm config images list --kubernetes-version=v1.11.2`
+do
+    download ${image}
 done
-IFS="$OLD_IFS"
-docker images
