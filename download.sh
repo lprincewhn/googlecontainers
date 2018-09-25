3c7b02a1549c95e1ae2a51f709601b4f1d6c333b
lprince <lprincewhn@gmail.com>
2018-09-25 15:42:39 +0800

    Move dockfiles to master branch.

---
+++ download.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+function download() {
+    image=$1
+    OLD_IFS="$IFS"
+    IFS="/"
+    arr=(${image})
+    if [ ${#arr[@]} -eq 3 ];
+    then
+        registry=${arr[0]}/${arr[1]}
+        name_version=${arr[2]}
+    else
+        registry=${arr[0]}
+        name_version=${arr[1]}
+    fi
+    IFS=":"
+    arr=(${name_version})
+    if [ ${#arr[@]} -eq 2 ];
+    then
+        name=${arr[0]}
+        tag=${arr[1]}
+    else
+        name=${name_version}
+        tag="latest"
+    fi
+    echo "docker pull lprince/googlecontainers:${name}-${tag}"
+    docker pull lprince/googlecontainers:${name}-${tag}
+    echo "docker tag lprince/googlecontainers:${name}-${tag} ${registry}/${name}:${tag}"
+    docker tag lprince/googlecontainers:${name}-${tag} ${registry}/${name}:${tag}
+    echo "docker rmi lprince/googlecontainers:${name}-${tag}"
+    docker rmi lprince/googlecontainers:${name}-${tag}
+    IFS="$OLD_IFS"
+}
--- get_kube_dashboard.sh
+++ get_kube_dashboard.sh
@@ -1,33 +1,6 @@
 #!/usr/bin/bash
 
-function download() {
-    image=$1
-    OLD_IFS="$IFS"
-    IFS="/"
-    arr=(${image})
-    if [ ${#arr[@]} -eq 3 ]
-    then
-        registry=${arr[0]}/${arr[1]}
-        name_version=${arr[2]}
-    else
-        registry=${arr[0]}
-        name_version=${arr[1]} 
-    fi
-    IFS=":"
-    arr=(${name_version})
-    if [ ${#arr[@]} -eq 2 ]
-    then
-        name=${arr[0]}
-        tag=${arr[1]}
-    else
-        name=${name_version}
-        tag="latest"
-    fi
-    echo "docker pull lprince/googlecontainers:${name}-${tag}"
-    docker pull lprince/googlecontainers:${name}-${tag}
-    echo "docker tag lprince/googlecontainers:${name}-${tag} ${registry}/${name}:${tag}"
-    docker tag lprince/googlecontainers:${name}-${tag} ${registry}/${name}:${tag}
-    IFS="$OLD_IFS"
-}
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+. ${DIR}/download.sh
 
 download "k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0"
